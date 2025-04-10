1649df709dd12391cf5d82e4dd4f50550ee5554a
nirokay <nirokay-public@protonmail.com>
2025-04-10 19:47:35 +0200

    trying to fix

--- storage/storage.lua
+++ storage/storage.lua
@@ -22,9 +22,12 @@ function storage.pushSlotToChests(slot)
 
     for name, chest in ipairs(chests.storage) do
         local pushedItems = chest.input.pushItems(name, slot)
+        print("Pushed " .. pushedItems .. " items")
         if chests.input.getItemDetail(slot) == nil or chests.input.getItemDetail(slot) == 0 then return end
     end
-    print("Could not push items!")
+    if chests.input.getItemDetail(slot) ~= nil and chests.input.getItemDetail(slot) ~= 0 then
+        print("Could not push items!")
+    end
 end
 
 function storage.pushAllItemsToChests()
