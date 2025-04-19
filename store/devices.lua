5ad3efe8f296efb1cae86ee214db649329c13296
nirokay <nirokay-public@protonmail.com>
2025-04-19 18:50:06 +0200

    fixed complete crash

--- store/devices.lua
+++ store/devices.lua
@@ -1,26 +1,28 @@
-local userdata = require("disk/userdata.lua")
+local pathUserData = "disk/userdata.lua"
 
-if userdata == nil then
+if not fs.exists(pathUserData) then
     if not fs.exists("disk") then
-        error("Uninitialised userdata.lua file, please insert disk into disk drive.")
+        error("Please insert disk into disk drive.")
     end
-    if not fs.exists("disk/userdata.lua") then
-        local template = io.open("templateUserData.lua", "r")
-        if template == nil then
-            error("User Data Template file does not exist, cannot initialise.")
-        end
-        local target = io.open("disk/userdata.lua", "w")
-        if target == nil then
-            error("Could not open file '/disk/userdata.lua' to write init.")
-            template:close()
-        end
-        target:write(template:read())
 
-        target:close()
+    local template = io.open("templateUserData.lua", "r")
+    if template == nil then
+        error("User Data Template file does not exist, cannot initialise.")
+    end
+    local target = io.open(pathUserData, "w")
+    if target == nil then
+        error("Could not open file '/disk/userdata.lua' to write init.")
         template:close()
     end
+    target:write(template:read())
+
+    target:close()
+    template:close()
+
     error("Initialised userdata file, please modify it (location: '/disk/userdata.lua')!")
 end
+
+local userdata = require(pathUserData)
 
 local device = {
     paymentInput = nil,
